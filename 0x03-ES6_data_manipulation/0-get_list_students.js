ef141c2d371f551bf592adcc165db49b1e131533
emziti <geologistelton@gmail.com>
2023-07-03 10:15:23 -0700

    all tasks

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+const getListStudents = () => [
+	{id: 1 , firstName: 'Guillaume' , location: 'SanFrancisco'},
+        {id: 2 , firstName: 'James' , location: 'Colombia'},
+        {id: 5 , firstName: 'Serena' , location: 'SanFrancisco'},
+
+]
+
+export default getListStudents;
+
